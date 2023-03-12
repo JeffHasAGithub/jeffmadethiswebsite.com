99b45caa34233d45d280e373bf4f8b34918ff2c1
Jeff Moore <jeffmooreweb@gmail.com>
2023-03-12 14:13:41 -0500

    Update create_app factory function to check for configuration files

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,23 @@
+import os
 from flask import Flask, render_template
 
 
-def create_app():
-    app = Flask(__name__)
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        KEY="dev",
+        DB=os.path.join(app.instance_path, "website.sqlite"),
+    )
+
+    if test_config is None:
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
 
     @app.route("/")
     def index():
