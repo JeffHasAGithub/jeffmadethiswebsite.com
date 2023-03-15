3a17f3dc9c36603a04141fd1343872161aff7260
Jeff Moore <jeffmooreweb@gmail.com>
2023-03-14 21:17:25 -0500

    Add 'click' event listener for 'pbtn' button

--- website/static/js/index.js
+++ website/static/js/index.js
@@ -3,6 +3,16 @@ let pi = 0;
 const dom = {
     pbtn:       document.getElementById("pbtn"),
     nbtn:       document.getElementById("nbtn"),
-    projects:   document.getElementsByClassName(""),
-
+    projects:   document.getElementsByClassName("project"),
 };
+
+dom.projects[pi].classList.add("active");
+
+dom.pbtn.addEventListener("click", _ => {
+    dom.projects[pi].classList.remove("active");
+
+    if (pi-- === 0)
+        pi = dom.projects.length - 1;
+
+    dom.projects[pi].classList.add("active");
+})
