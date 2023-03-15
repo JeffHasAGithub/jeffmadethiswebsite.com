f6b18ad0a32f6296e3f0484e140f9fdaa6f9accd
Jeff Moore <jeffmooreweb@gmail.com>
2023-03-14 21:03:17 -0500

    Add global 'dom' object for holding elements used in index.js script

--- website/static/js/index.js
+++ website/static/js/index.js
@@ -1,1 +1,8 @@
 let pi = 0;
+
+const dom = {
+    pbtn:       document.getElementById("pbtn"),
+    nbtn:       document.getElementById("nbtn"),
+    projects:   document.getElementsByClassName(""),
+
+};
