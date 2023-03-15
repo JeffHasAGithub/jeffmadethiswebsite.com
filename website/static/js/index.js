0aaca9c34f6fc4f33a7fa8d5c351f0bd066efb22
Jeff Moore <jeffmooreweb@gmail.com>
2023-03-14 22:03:07 -0500

    Update projects (#14)
    * Pass list of projects to index template
    
    * Add basic tags and structure to projects section in index template
    
    * Move project text content to <div> tag
    
    * Move active project into <div> tag
    
    * Add separate 'link' icon for links to project URLs
    
    * Add buttons for cycling through projects in index template
    
    * Add styling for projects section in index template
    
    * Add index.js script to index template
    
    * Add project image for Arctic Express Pack to static files
    
    * Update index template to load projects from Sqlite database
    
    * Update styling for index template
    
    * Hide projects by default in index template
    
    * Add id propert to projects buttons in index template
    
    * Add projecct class to projects in index template
    
    * Change 'project' class name to 'active'
    
    * Change 'active' classname back to 'project'
    
    * Add 'active' class for display the current project
    
    * Add 'pi' variable for tracking current active project
    
    * Add global 'dom' object for holding elements used in index.js script
    
    * Add 'defer' attribute to <script> tag in index template
    
    * Add 'click' event listener for 'pbtn' button
    
    * Add 'click' event listener for 'nbtn' button
    
    * Add responsive styling to 'projects' section in index template
    
    * Move buttons for cycling projects to the top of the 'projects' section in the index template:

---
+++ website/static/js/index.js
@@ -0,0 +1,27 @@
+let pi = 0;
+
+const dom = {
+    pbtn:       document.getElementById("pbtn"),
+    nbtn:       document.getElementById("nbtn"),
+    projects:   document.getElementsByClassName("project"),
+};
+
+dom.projects[pi].classList.add("active");
+
+dom.pbtn.addEventListener("click", _ => {
+    dom.projects[pi].classList.remove("active");
+
+    if (pi-- === 0)
+        pi = dom.projects.length - 1;
+
+    dom.projects[pi].classList.add("active");
+});
+
+dom.nbtn.addEventListener("click", _ => {
+    dom.projects[pi].classList.remove("active");
+
+    if (++pi == dom.projects.length)
+        pi = 0;
+
+    dom.projects[pi].classList.add("active");
+});
