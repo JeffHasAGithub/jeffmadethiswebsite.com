524a9c6c47d5472c7fef481a5957af135dd49bfa
Jeff Moore <jeffmooreweb@gmail.com>
2023-03-19 15:16:37 -0500

    Add side nav (#15)
    * Add <header> tag to base template
    
    * Add <div> tag for toggling nav in base template
    
    * Remove styling that auto centers <header> and <footer> tags with <main> tag
    
    * Change 'body > *' style rule in media queries to 'main'
    
    * Set text color to white in <header> tag of base template
    
    * Set cursor to 'pointer' in <header> tag of base template
    
    * Add <nav> child to <header> tag in base template
    
    * Add links to <nav> tag in base template
    
    * Add <div> child for closing <nav> tag
    
    * Add styling to <header> tag and its children
    
    * Add 'open-nav' ID to <div> child of <header> tag
    
    * Add 'close-nav' ID to <div> child of <nav> tag
    
    * Empty old nav code in base script
    
    * Add 'sidenav' ID to <nav> tag in base template
    
    * Add 'defer' attribute to <script> tag in base template
    
    * Add functionality for opening sidenav in base script
    
    * Set width of sidenav to '0' by default in base template
    
    * Add functionality for closing sidenav in base template
    
    * Add 'transition' rule to sidenav in base template
    
    * Add 'font-size' to <header> tag in base template

--- website/static/js/base.js
+++ website/static/js/base.js
@@ -1,7 +1,11 @@
-function toggleNavbar() {
-    const nav = document.getElementsByTagName("nav")[0];
-    if (nav.className === "toggle")
-        nav.className = "";
-    else
-        nav.className = "toggle";
-}
+const sidenav = document.getElementById("sidenav");
+const navopen = document.getElementById("open-nav");
+const navclose = document.getElementById("close-nav");
+
+navopen.addEventListener("click", _ => {
+    sidenav.style.width = "100vw";
+});
+
+navclose.addEventListener("click", _ => {
+    sidenav.style.width = "0vw";
+});
