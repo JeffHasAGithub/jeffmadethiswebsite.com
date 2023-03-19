2b0150123fe9456b1078b2202f8f1192fe972035
Jeff Moore <jeffmooreweb@gmail.com>
2023-03-19 14:42:04 -0500

    Empty old nav code in base script

--- website/static/js/base.js
+++
@@ -1,7 +0,0 @@
-function toggleNavbar() {
-    const nav = document.getElementsByTagName("nav")[0];
-    if (nav.className === "toggle")
-        nav.className = "";
-    else
-        nav.className = "toggle";
-}
