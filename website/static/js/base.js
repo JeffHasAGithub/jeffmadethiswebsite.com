30f7b82efa5d5066dced271e5e4cb8c81afecec0
Jeff Moore <jeffmooreweb@gmail.com>
2023-03-19 15:05:16 -0500

    Add functionality for closing sidenav in base template

--- website/static/js/base.js
+++ website/static/js/base.js
@@ -1,6 +1,11 @@
 const sidenav = document.getElementById("sidenav");
 const navopen = document.getElementById("open-nav");
+const navclose = document.getElementById("close-nav");
 
 navopen.addEventListener("click", _ => {
     sidenav.style.width = "100vw";
+});
+
+navclose.addEventListener("click", _ => {
+    sidenav.style.width = "0vw";
 });
