4dc50e789574a45649f14aed83f58ee5eebd9d0b
Jeff Moore <jeffmooreweb@gmail.com>
2023-03-20 18:41:19 -0500

    Implement '/projects/<int>' route in 'projects' blueprint

--- website/projects.py
+++ website/projects.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, abort
+from website.db import get_db
 
 bp = Blueprint("projects", __name__, url_prefix="/projects")
 
@@ -10,4 +11,11 @@ def projects():
 
 @bp.route("/<int:id>")
 def project(id):
-    pass
+    db = get_db()
+    project = db.execute("SELECT * FROM project"
+                         "WHERE project.id = ?").fetchone()
+
+    if project is None:
+        abort(404, f"Project {id} does not exist")
+
+    return render_template("project.html")
