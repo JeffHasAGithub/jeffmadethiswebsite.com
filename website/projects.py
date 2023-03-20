6b362530ffe7a296ab2c5b1740471f2418a2c2ca
Jeff Moore <jeffmooreweb@gmail.com>
2023-03-20 18:28:05 -0500

    Add '/projects' url prefix to 'projects' blueprint

--- website/projects.py
+++ website/projects.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, render_template
 
-bp = Blueprint("projects", __name__)
+bp = Blueprint("projects", __name__, url_prefix="/projects")
 
 
-@bp.route("/projects")
+@bp.route("/")
 def projects():
     return render_template("projects.html")
