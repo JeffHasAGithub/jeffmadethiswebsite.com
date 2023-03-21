cf34567c7a3624743e5084cc84cbc3abd92d9ac2
Jeff Moore <jeffmooreweb@gmail.com>
2023-03-20 20:40:25 -0500

    Pass named 'err' param to render_template func in 'page_not_found' route

--- website/errors.py
+++ website/errors.py
@@ -5,4 +5,4 @@ bp = Blueprint("errors", __name__)
 
 @bp.app_errorhandler(404)
 def page_not_found(err):
-    return render_template("errors/404.html")
+    return render_template("errors/404.html", err=err)
