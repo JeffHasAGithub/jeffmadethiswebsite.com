5c55c43f15f4be3110e8cf932f0b7e1532acdc21
Jeff Moore <jeffmooreweb@gmail.com>
2023-03-21 20:51:46 -0500

    Refactor app factory (#21)
    * Move route setup into setup_routes function
    
    * Move config setup into setup_config function
    
    * Move database setup into setup_database function
    
    * Move blueprints setup into setup_blueprints function
    
    * Move 'os' import into setup_config function
    
    * Cleanup module-level imports
    
    * Cleanup module-level imports in projects module

--- website/__init__.py
+++ website/__init__.py
@@ -1,12 +1,22 @@
-import os
+import flask
 import werkzeug
-from flask import Flask, render_template
-
-from website.db import get_db
+import website
 
 
 def create_app(test_config=None):
-    app = Flask(__name__, instance_relative_config=True)
+    app = flask.Flask(__name__, instance_relative_config=True)
+
+    setup_config(app, test_config)
+    setup_routes(app)
+    setup_database(app)
+    setup_blueprints(app)
+
+    return app
+
+
+def setup_config(app, test_config):
+    import os
+
     app.config.from_mapping(
         KEY="dev",
         DB=os.path.join(app.instance_path, "website.sqlite"),
@@ -22,25 +32,29 @@ def create_app(test_config=None):
     except OSError:
         pass
 
+
+def setup_routes(app):
     @app.errorhandler(werkzeug.exceptions.HTTPException)
     def error(err):
-        return render_template("error.html", err=err), err.code
+        return flask.render_template("error.html", err=err), err.code
 
     @app.route("/")
     def index():
-        db = get_db()
+        db = website.db.get_db()
         projects = db.execute("SELECT * FROM project").fetchall()
 
-        return render_template("index.html", projects=projects)
+        return flask.render_template("index.html", projects=projects)
 
     @app.route("/resume")
     def resume():
-        return render_template("resume.html")
+        return flask.render_template("resume.html")
 
+
+def setup_database(app):
     from . import db
     db.register_db(app)
 
+
+def setup_blueprints(app):
     from . import projects
     app.register_blueprint(projects.bp)
-
-    return app
