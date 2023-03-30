cd3cd8aed3b1f00ac03c1875b71cb0669068883e
Jeff Moore <jeffmooreweb@gmail.com>
2023-03-29 19:02:50 -0500

    Add '/blog' and '/blog/<int:id>' routes to blog blueprint

--- website/blog.py
+++ website/blog.py
@@ -2,3 +2,13 @@ import flask
 import website
 
 bp = flask.Blueprint("blog", __name__, url_prefix="/blog")
+
+
+@bp.route("/")
+def blog():
+    pass
+
+
+@bp.route("/<int:id>")
+def post():
+    pass
