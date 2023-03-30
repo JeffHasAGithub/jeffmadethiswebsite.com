ec7c28822c3f71f82a95875f1287d0e8af622fa6
Jeff Moore <jeffmooreweb@gmail.com>
2023-03-29 19:07:29 -0500

    Implement 'blog' function for '/blog' route

--- website/blog.py
+++ website/blog.py
@@ -6,7 +6,13 @@ bp = flask.Blueprint("blog", __name__, url_prefix="/blog")
 
 @bp.route("/")
 def blog():
-    pass
+    db = website.db.get_db()
+    blog = db.execute("SELECT * FROM blog").fetchall()
+
+    if blog is None:
+        flask.abort(404, "could not find blog")
+
+    return flask.render_template("blog/index.html", blog=blog)
 
 
 @bp.route("/<int:id>")
