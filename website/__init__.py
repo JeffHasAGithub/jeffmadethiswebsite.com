444b4e3fa9d4a390724cf4bab3be27b6708af7e5
Jeff Moore <jeffmooreweb@gmail.com>
2023-03-31 21:04:24 -0500

    Rename call to setup_routes() in create_app() definition to setup_errorhandlers()

--- website/__init__.py
+++ website/__init__.py
@@ -7,9 +7,9 @@ def create_app(test_config=None):
     app = flask.Flask(__name__, instance_relative_config=True)
 
     setup_config(app, test_config)
-    setup_routes(app)
     setup_database(app)
     setup_blueprints(app)
+    setup_errorhandlers(app)
 
     return app
 
